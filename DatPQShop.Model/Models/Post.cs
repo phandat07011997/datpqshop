10c0a6010cc0b955b92a147919191b936d42dd29
DESKTOP-78FJB0E\DatPQ <phandat07011997@gmai.com>
2018-07-17 10:25:03 +0700

    fix bugs for repository and add post service

--- DatPQShop.Model/Models/Post.cs
+++ DatPQShop.Model/Models/Post.cs
@@ -1,4 +1,5 @@
 ﻿using DatPQShop.Model.Abstract;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
@@ -37,5 +38,7 @@ namespace DatPQShop.Model.Models
         
         [ForeignKey("CategoryID")]
         public virtual PostCategory PostCategory { set; get; }
+
+        public virtual IEnumerable<PostTag> PostTags { set; get; }
     }
 }
