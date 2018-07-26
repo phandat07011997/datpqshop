3ec116f9cd2f9b49d96a079d5298fe8ee6f6ff7a
DESKTOP-78FJB0E\DatPQ <phandat07011997@gmai.com>
2018-07-26 15:05:39 +0700

    add unit test for repository and service

---
+++ DatPQShop.UnitTest/RepositoryTest/PostCategoryRepositoryTest.cs
@@ -0,0 +1,42 @@
+﻿using DatPQShop.Data.Infrastructure;
+using DatPQShop.Data.Repositories;
+using DatPQShop.Model.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+
+namespace DatPQShop.UnitTest.RepositoryTest
+{
+    [TestClass]
+    public class PostCategoryRepositoryTest
+    {
+        IDbFactory dbFactory;
+        IPostCategoryRepository objRepository;
+        IUnitOfWork unitOfWork;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            dbFactory = new DbFactory();
+            objRepository = new PostCategoryRepository(dbFactory);
+            unitOfWork = new UnitOfWork(dbFactory);
+        }
+        [TestMethod]
+        public void PostCategory_Repository_GetAll()
+        {
+            var list = objRepository.GetAll().ToList();
+            Assert.AreEqual(3, list.Count());
+        }
+        [TestMethod]
+        public void PostCategory_Repository_Create()
+        {
+            PostCategory category = new PostCategory();
+            category.Name = "Test category";
+            category.Alias = "Test-category";
+            category.Status = true;
+            var result = objRepository.Add(category);
+            unitOfWork.Commit();
+            Assert.IsNotNull(result);
+            Assert.AreEqual(3, result.ID);
+        }
+    }
+}
