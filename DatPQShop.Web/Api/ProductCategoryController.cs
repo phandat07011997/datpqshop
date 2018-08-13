be14a9aaba4989937511fe7523a6fbe2df549043
DESKTOP-78FJB0E\DatPQ <phandat07011997@gmai.com>
2018-08-13 15:49:06 +0700

    add paging

--- DatPQShop.Web/Api/ProductCategoryController.cs
+++ DatPQShop.Web/Api/ProductCategoryController.cs
@@ -3,7 +3,9 @@ using DatPQShop.Model.Models;
 using DatPQShop.Service;
 using DatPQShop.Web.Infrastructure.Core;
 using DatPQShop.Web.Models;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
@@ -20,15 +22,28 @@ namespace DatPQShop.Web.Api
             this._productCategoryService = productCategoryService;
         }
         [Route("getall")]
-        public HttpResponseMessage GetAll(HttpRequestMessage request)
+        public HttpResponseMessage GetAll(HttpRequestMessage request,int page, int pageSize=20)
         {
             return CreateHttpResponse(request, () =>
             {
+                int totalRow = 0;
                 var model = _productCategoryService.GetAll();
-                var responseData = Mapper.Map<IEnumerable<ProductCategory>,IEnumerable< ProductCategoryViewModel >>(model);
-                var response = request.CreateResponse(HttpStatusCode.OK, responseData);
+                totalRow = model.Count();
+                var query = model.OrderByDescending(x => x.CreatedDate).Skip(page * pageSize).Take(pageSize);
+                var responseData = Mapper.Map<IEnumerable<ProductCategory>, IEnumerable<ProductCategoryViewModel>>(query);
+                var paginationSet = new PaginationSet<ProductCategoryViewModel>()
+                {
+                    Items = responseData,
+                    Page = page,
+                    TotalCount = totalRow,
+                    TotalPages = (int)Math.Ceiling((decimal)totalRow / pageSize)
+                };
+
+
+                var response = request.CreateResponse(HttpStatusCode.OK, paginationSet);
                 return response;
             });
         }
+
     }
 }
--- DatPQShop.Web/app/components/product_categories/productCategoryListController.js
+++ DatPQShop.Web/app/components/product_categories/productCategoryListController.js
@@ -3,11 +3,24 @@
     app.controller('productCategoryListController', productCategoryListController);
     productCategoryListController.$inject = ['$scope','apiService'];
     function productCategoryListController($scope,apiService) {
+        $scope.page = 0;
+        $scope.pageCount = 0;
         $scope.productCategories = [];
         $scope.getProductCategories = getProductCategories;
-        function getProductCategories() {
-            apiService.get('/api/productcategory/getall', null, function (result) {
-                $scope.productCategories = result.data;
+        function getProductCategories(page) {
+            page = page || 0;
+            var config = {
+                params: {
+                    page: page,
+                    pageSize: 2
+                }
+            }
+            apiService.get('/api/productcategory/getall', config, function (result) {
+                $scope.productCategories = result.data.Items;
+                $scope.page = result.data.Page;
+                $scope.pagesCount = result.data.TotalPages;
+                $scope.totalCount = result.data.TotalCount;
+
             }, function () {
                 console.log('load product category failed.')
             });
