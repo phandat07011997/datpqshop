bdd455fc4bb579eac76e0c8ccc8de9cf26491e94
DESKTOP-78FJB0E\DatPQ <phandat07011997@gmai.com>
2018-08-18 16:22:20 +0700

    add notification service

--- DatPQShop.Web/app/components/product_categories/productCategoryListController.js
+++ DatPQShop.Web/app/components/product_categories/productCategoryListController.js
@@ -1,8 +1,8 @@
 ﻿/// <reference path="C:\Users\DatPQ\Documents\datpqshop\DatPQShop.Web\Assets/admin/libs/angular/angular.js" />
 (function (app) {
     app.controller('productCategoryListController', productCategoryListController);
-    productCategoryListController.$inject = ['$scope','apiService'];
-    function productCategoryListController($scope,apiService) {
+    productCategoryListController.$inject = ['$scope','apiService','notificationService'];
+    function productCategoryListController($scope, apiService, notificationService) {
         $scope.page = 0;
         $scope.pageCount = 0;
         $scope.productCategories = [];
@@ -18,10 +18,16 @@
                 params: {
                     keyword: $scope.keyword,
                     page: page,
-                    pageSize: 2
+                    pageSize: 10
                 }
             }
             apiService.get('/api/productcategory/getall', config, function (result) {
+                if (result.data.TotalCount == 0) {
+                    notificationService.displayWarning('Không có bản ghi nào được tìm thấy.');
+                }
+                else {
+                    notificationService.displaySuccess('Đã tìm thấy ' + result.data.TotalCount + ' bản ghi.');
+                }
                 $scope.productCategories = result.data.Items;
                 $scope.page = result.data.Page;
                 $scope.pagesCount = result.data.TotalPages;
