b836be56dcddaa3ec87e40637aebdfdb330f7921
DESKTOP-78FJB0E\DatPQ <phandat07011997@gmai.com>
2018-08-26 22:17:15 +0700

    add sort product

--- DatPQShop.Service/ProductService.cs
+++ DatPQShop.Service/ProductService.cs
@@ -28,7 +28,7 @@ namespace DatPQShop.Service
 
         IEnumerable<Product> GetHotProduct(int top);
 
-        IEnumerable<Product> GetListProductByCategoryIdPaging(int categoryId,int page,int pageSize,out int totalRow);
+        IEnumerable<Product> GetListProductByCategoryIdPaging(int categoryId,int page,int pageSize,out int totalRow,string sort);
 
         void Save();
     }
@@ -141,9 +141,24 @@ namespace DatPQShop.Service
             return _ProductRepository.GetMulti(x => x.Status == true && x.HotFlag == true).OrderByDescending(x => x.CreatedDate).Take(top);
         }
 
-        public IEnumerable<Product> GetListProductByCategoryIdPaging(int categoryId, int page, int pageSize, out int totalRow)
+        public IEnumerable<Product> GetListProductByCategoryIdPaging(int categoryId, int page, int pageSize, out int totalRow,string sort)
         {
             var query = _ProductRepository.GetMulti(x => x.Status==true && x.CategoryID == categoryId);
+            switch (sort)
+            {
+                case "popular":
+                    query = query.OrderByDescending(x => x.ViewCount);
+                    break;
+                case "discount":
+                    query = query.OrderByDescending(x => x.PromotionPrice.HasValue);
+                    break;
+                case "price":
+                    query = query.OrderBy(x => x.Price);
+                    break;
+                default:
+                    query = query.OrderByDescending(x => x.CreatedDate);
+                    break;
+            }
             totalRow = query.Count();
             return query.Skip((page - 1) * pageSize).Take(pageSize);
         }
